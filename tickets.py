ddf840c44bc771cc05783469db10b2b0223c3f1a
Zeuxsf <alexandrefranca270324@gmail.com>
2026-02-11 11:11:36 -0300

    finalização da função 'abrir ticket'

--- tickets.py
+++ tickets.py
@@ -56,11 +56,39 @@ def gerar_ticket_randômico():
     return ticket_gerado
 
 def abrir_ticket(nome,user_email,assunto,mensagem):
+    nome = nome.lower()
+    user_email = user_email.lower()
+
+    session = SessionLocal()
+    #Esse script vai verificar ser o usuário já existe no sistema, se ele não existir, vai adicionar no banco de dados automaticamente. Pretendo colocar uma verificação de username na função de [visualizar ticket], pra caso exista usuários de nomes diferentes porém com o mesmo email
+    user = session.query(Client).filter(Client.name == nome, Client.email == user_email).first()
+    if not user:
+        new_user = Client(nome,user_email)
+        session.add(new_user)
+        session.commit()
+        user = session.query(Client).filter(Client.name == nome, Client.email == user_email).first()            
+    if user:
+        print('usuário já existe') #debug    
+
     ticket = gerar_ticket_randômico()
+    
+    adicionar_ticket = Ticket(user.id,ticket,assunto)
+    session.add(adicionar_ticket)
+    session.commit()
 
     enviar_email(nome,user_email,assunto,mensagem,ticket)
+    session.close()
+    return ticket #Pra api poder retornar o ticket que o usuário deve guardar para obter a resposta
 
 if __name__ == '__main__':
-    #abrir_ticket('Alexandre','alexandrefranca270324@gmail.com','OTESTE2','Cara, você é foda demais, meu sonho é ser igual você um dia')
+    #abrir_ticket('Alexandre','alexandrefranca270324@gmail.com','ovo','Caro programador, estamos felizes de te contratar, você começa amanhã como CEO') #Tão deixando a gente sonhar kkkkkk
     #print(gerar_ticket_randômico())
-    ...
+    
+    '''
+    session = SessionLocal()
+    item = session.query(Client).filter(Client.id == 12).first() #Código pra excluir usuários defeituosos
+    session.delete(item)
+    print('excluido')
+    session.commit()
+    session.close()
+    '''
