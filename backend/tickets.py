12dc0878dd93f4a05ffbb07a723acad7a23e2226
Alexandre S. de França <alexandrefranca270324@gmail.com>
2026-02-20 13:23:22 -0300

    Atualizar o tickets.py

--- backend/tickets.py
+++ backend/tickets.py
@@ -31,7 +31,7 @@ def enviar_email(nome,user_email,assunto,mensagem,ticket):  #Quem diria, aprendi
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email )
 
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s = smtplib.SMTP('smtp.gmail.com', 465, timeout=20)
     s.starttls()
 
     s.login(msg['From'], password)
@@ -60,7 +60,7 @@ def enviar_email_usuario(nome,user_email,assunto,ticket):  #Essa função vai ma
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email )
 
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s = smtplib.SMTP('smtp.gmail.com', 465, timeout=20)
     s.starttls()
 
     s.login(msg['From'], password)
